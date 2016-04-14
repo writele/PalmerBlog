dcf2418bee471ed2ec629878900bcd3240cc389d
Ele <ecpalmer21@gmail.com>
2016-04-14 14:17:20 -0400

    JS implemented so Edit comment only works on one comment

--- PalmerBlog/Scripts/main.js
+++ PalmerBlog/Scripts/main.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
 
     function EditComment() {
-        $("comment-content-edit").show();
-        $(".comment-btn-edit").hide();
-        $(".comment-content").hide();
-        console.log($(this).parent().find("comment-content-edit"));
+        $(this).parent().find(".comment-content-edit").toggle();
+        $(".comment-btn-edit").toggle();
+        $(".comment-btn-delete").toggle();
+        $(".comment-btn-cancel").toggle();
+        $(".comment-content").toggle();
     };
 
     $(".comment-btn-edit").on("click", EditComment);
+    $(".comment-btn-cancel").on("click", EditComment);
 });
 
 
