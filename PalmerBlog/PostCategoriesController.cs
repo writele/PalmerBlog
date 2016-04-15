ef681a68497d6a05f47a14ef20fee87d9b53d20d
Ele <ecpalmer21@gmail.com>
2016-04-15 13:35:31 -0400

    Scaffolded PostCategories

---
+++ PalmerBlog/PostCategoriesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PalmerBlog.Models;
+
+namespace PalmerBlog
+{
+    public class PostCategoriesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: PostCategories
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        // GET: PostCategories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostCategory postCategory = db.Categories.Find(id);
+            if (postCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postCategory);
+        }
+
+        // GET: PostCategories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PostCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Slug")] PostCategory postCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(postCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(postCategory);
+        }
+
+        // GET: PostCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostCategory postCategory = db.Categories.Find(id);
+            if (postCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postCategory);
+        }
+
+        // POST: PostCategories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Slug")] PostCategory postCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(postCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(postCategory);
+        }
+
+        // GET: PostCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostCategory postCategory = db.Categories.Find(id);
+            if (postCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postCategory);
+        }
+
+        // POST: PostCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PostCategory postCategory = db.Categories.Find(id);
+            db.Categories.Remove(postCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
