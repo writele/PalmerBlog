072678447d774f6b68e69de2ee49cbc58a221e1c
Ele <ecpalmer21@gmail.com>
2016-04-15 15:17:29 -0400

    Added welcome message to index, link option to TinyMCE

---
+++ PalmerBlog/Migrations/201604141806342_displayname set.cs
@@ -0,0 +1,16 @@
+namespace PalmerBlog.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class displaynameset : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
